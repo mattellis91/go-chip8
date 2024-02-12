eda29cae5e27c21ea57aada115ba6b6477824542
Matt Ellis <mathew.ellis2@griffithuni.edu.au>
2024-02-12 15:36:06 +1000

    added content address locations

--- main.go
+++ main.go
@@ -8,10 +8,24 @@ import (
 	"github.com/hajimehoshi/ebiten/v2"
 )
 
+const (
+	PcStartAddress = 0x200
+	FontStartAddress = 0x50
+)
+
 type Game struct {
 	display Display
 	cpu CPU
 }
+
+//INPUT KEYS
+
+//Chip8 	//Keyboard
+// 1 2 3 C  | 1 2 3 4
+// 4 5 6 D  | Q W E R
+// 7 8 9 E	| A S D F 
+// A 0 B F	| Z X C V
+
 
 func (g *Game) Update() error {
 
